86355fc09c7eb546e65c8e515ea6a1cef45a70a7
Shannon Nichols <shannonsn19@gmail.com>
2017-05-18 11:18:27 +0200

    renders the u  // if(textbox !== ''){sername looked up and the number of repositories they have in github

--- app.js
+++ app.js
@@ -3,14 +3,11 @@ var submitButton = document.getElementById('submit');
 var display = document.getElementById('display');
 
 function submit(){
-  // if(textbox !== ''){
-  // display.innerHTML = "https://github.com/" + textbox.value
-  //  }
   var url= "http://api.github.com/users/" + textbox.value;
   //makes the ajax call to the github api
   // alert($);
   $.get(url).then(function(result){
     //result the response from the github call
-    display.innerHTML = result.public_repos;
-  })
+    display.innerHTML = "Hi " + textbox.value + " you have " + result.public_repos + " github repository/repositories";
+  });
 };
